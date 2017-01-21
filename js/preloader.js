1c0d9a690a0c77bc36cdb352b79a0d128ac32810
Will Fetzer <redstoneftw1234@gmail.com>
2017-01-20 19:10:53 -0800

    Updates and Bugfixes
    Fixed Bug With Image Tiling
    Added Preloader
    Added Downloads Menu

--- js/preloader.js
+++ js/preloader.js
@@ -2,5 +2,7 @@
  * Created by RAVE on 1/20/2017.
  */
 $(document).ready(function() {
-    $('body').addClass('loaded');
+    //$('body').addClass('loaded').css("background-color","white");
+    //document.getElementById("load").style.display = "none";
+    //document.getElementById("makeinvisible").style.display = "block";
 });
