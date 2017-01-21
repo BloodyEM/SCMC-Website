2142f74bbf0f887fab07a43bb4ee809c02bbccd3
Will Fetzer <redstoneftw1234@gmail.com>
2017-01-20 20:54:05 -0800

    Last update for tonight

--- js/collapse.js
+++ js/collapse.js
@@ -2,5 +2,8 @@
  * Created by RAVE on 1/20/2017.
  */
 function downloadsToggle() {
-    $("#versions").toggle("collapse")
+    $("#versions").slideToggle('slow');
+}
+function teamToggle() {
+    $("#team").slideToggle('slow');
 }
