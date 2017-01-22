afd5a8d76e74558aa1dbbd2ec815eb0b04ca3ea7
Will Fetzer <redstoneftw1234@gmail.com>
2017-01-21 19:54:41 -0800

    Added social buttons

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var banner = ['/*!\n',
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-    return gulp.src('less/new-age.less')
+    return gulp.src('less/style.less')
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
@@ -53,13 +53,13 @@ gulp.task('minify-js', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('vendor/bootstrap'));;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('vendor/jquery'));;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     gulp.src(['node_modules/simple-line-icons/*/*'])
-        .pipe(gulp.dest('vendor/simple-line-icons'))
+        .pipe(gulp.dest('vendor/simple-line-icons'));;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
     gulp.src([
@@ -71,7 +71,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.json'
         ])
         .pipe(gulp.dest('vendor/font-awesome'))
-})
+});;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 // Run everything
 gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
@@ -83,7 +83,7 @@ gulp.task('browserSync', function() {
             baseDir: ''
         },
     })
-})
+});;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
--- js/collapse.js
+++ js/collapse.js
@@ -1,9 +1,15 @@
 /**
  * Created by RAVE on 1/20/2017.
  */
-function downloadsToggle() {
-    $("#versions").slideToggle('slow');
-}
 function teamToggle() {
     $("#team").slideToggle('slow');
+}
+function v1() {
+    $("#versiondownloads1").slideToggle("slow");
+    $("#versiondownloads2").slideUp("slow");
+}
+function v2() {
+    console.log("version not released yet!");
+    $("#versiondownloads2").slideToggle("slow");
+    $("#versiondownloads1").slideUp("slow");
 }
