2c3d2242e35c1ffa3ea5e881bdaa240c6695b043
Will Fetzer <redstoneftw1234@gmail.com>
2017-01-21 21:56:32 -0800

    Tempfix for preloader
    Put a 3 second requirement on the loading screen until I can make it
    actually work

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -2,8 +2,10 @@
  * Created by RAVE on 1/20/2017.
  */
 $(document).ready(function() {
-    $("#load").fadeOut(500, function() {
-        document.getElementById("makeinvisible").style.opacity = "1";
-        $('body').css("background-color", "white");
-    });
+    setTimeout(function() {
+        $("#load").fadeOut(500, function() {
+            document.getElementById("makeinvisible").style.opacity = "1";
+            $('body').css("background-color", "white");
+        });
+    }, 3000);
 });
