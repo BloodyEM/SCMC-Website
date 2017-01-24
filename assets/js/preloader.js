8d4e3f14c2732949e40c74b00a6600e92cb7237c
Will Fetzer <redstoneftw1234@gmail.com>
2017-01-23 20:07:02 -0800

    Added more people to "Meet the team"

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -2,8 +2,10 @@
  * Created by RAVE on 1/20/2017.
  */
 $(document).ready(function() {
+    setTimeout(function() {
         $("#load").fadeOut(500, function() {
             document.getElementById("makeinvisible").style.opacity = "1";
             $('body').addClass("loaded");
         });
+    }, 2);
 });
