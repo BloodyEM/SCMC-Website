742ae863364429f1fb58dc3a4f41677ab0a8e3d2
Will Fetzer <redstoneftw1234@gmail.com>
2017-02-11 20:29:09 -0800

    you know...
    I should really test this stuff before I roll out changes

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,9 +1,11 @@
 /**
  * Created by RAVE on 1/20/2017.
  */
-$(window).load(function() {
-    $("#load").fadeOut(500, function() {
-        document.getElementById("makeinvisible").style.opacity = "1";
-        $('body').addClass("loaded");
-    });
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#load").fadeOut(500, function() {
+            document.getElementById("makeinvisible").style.opacity = "1";
+            $('body').addClass("loaded");
+        });
+    }, 1000);
 });
